bb018365a5d0c238df7803aad89b14b51dede6f5
Antonio Duarte <aduarte496@yahoo.com>
2024-01-17 05:56:39 -0600

    prob01: c++: done

---
+++ prob01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cin >> name;
+    std::cout << name << ", the needs of the many outweigh the needs of the few, or the one; live long and prosper." << std::endl;
+    return 0;
+}
